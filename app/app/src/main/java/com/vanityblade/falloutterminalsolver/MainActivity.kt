ccf4fe5860acb405e0de6b9143dae78e3ff5e13b
Connor Garcia <connoregarcia@gmail.com>
2022-06-22 20:46:55 -0500

    Added skeleton of layout

--- app/app/src/main/java/com/vanityblade/falloutterminalsolver/MainActivity.kt
+++ app/app/src/main/java/com/vanityblade/falloutterminalsolver/MainActivity.kt
@@ -2,10 +2,25 @@ package com.vanityblade.falloutterminalsolver
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+
+private const val TAG: String = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.optionOCR).setOnClickListener { optionOCRClicked() }
+        findViewById<Button>(R.id.optionManual).setOnClickListener { optionManualClicked() }
+    }
+
+    private fun optionManualClicked() {
+        Log.v(TAG, "manual was clicked")
+    }
+
+    private fun optionOCRClicked() {
+        Log.v(TAG, "OCR was clicked")
     }
 }
